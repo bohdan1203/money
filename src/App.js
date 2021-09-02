dc7833c104266badfdecfc8f60fe5a520cf27b3f
Bohdán Katsenko <80700199+bohdan1203@users.noreply.github.com>
2021-09-02 20:15:14 +0300

    dksf

--- src/App.js
+++ src/App.js
@@ -6,7 +6,20 @@ import "./App.css";
 
 function App() {
     return (
-    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, voluptatem! Cumque cupiditate nostrum ducimus alias cum assumenda eius id, vitae explicabo, debitis eaque voluptas aperiam reprehenderit sunt vel error quo.</div>
+      <div>
+        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, voluptatem! Cumque cupiditate nostrum ducimus alias cum assumenda eius id, vitae explicabo, debitis eaque voluptas aperiam reprehenderit sunt vel error quo.</div>
+        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, voluptatem! Cumque cupiditate nostrum ducimus alias cum assumenda eius id, vitae explicabo, debitis eaque voluptas aperiam reprehenderit sunt vel error quo.</div>
+        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, voluptatem! Cumque cupiditate nostrum ducimus alias cum assumenda eius id, vitae explicabo, debitis eaque voluptas aperiam reprehenderit sunt vel error quo.</div>
+        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, voluptatem! Cumque cupiditate nostrum ducimus alias cum assumenda eius id, vitae explicabo, debitis eaque voluptas aperiam reprehenderit sunt vel error quo.</div>
+        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, voluptatem! Cumque cupiditate nostrum ducimus alias cum assumenda eius id, vitae explicabo, debitis eaque voluptas aperiam reprehenderit sunt vel error quo.</div>
+        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, voluptatem! Cumque cupiditate nostrum ducimus alias cum assumenda eius id, vitae explicabo, debitis eaque voluptas aperiam reprehenderit sunt vel error quo.</div>
+        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, voluptatem! Cumque cupiditate nostrum ducimus alias cum assumenda eius id, vitae explicabo, debitis eaque voluptas aperiam reprehenderit sunt vel error quo.</div>
+        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, voluptatem! Cumque cupiditate nostrum ducimus alias cum assumenda eius id, vitae explicabo, debitis eaque voluptas aperiam reprehenderit sunt vel error quo.</div>
+        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, voluptatem! Cumque cupiditate nostrum ducimus alias cum assumenda eius id, vitae explicabo, debitis eaque voluptas aperiam reprehenderit sunt vel error quo.</div>
+        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, voluptatem! Cumque cupiditate nostrum ducimus alias cum assumenda eius id, vitae explicabo, debitis eaque voluptas aperiam reprehenderit sunt vel error quo.</div>
+        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, voluptatem! Cumque cupiditate nostrum ducimus alias cum assumenda eius id, vitae explicabo, debitis eaque voluptas aperiam reprehenderit sunt vel error quo.</div>
+        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, voluptatem! Cumque cupiditate nostrum ducimus alias cum assumenda eius id, vitae explicabo, debitis eaque voluptas aperiam reprehenderit sunt vel error quo.</div>
+      </div>
   );
 }
 
