9abc39e47708d309ca91b61f3532b2a475028941
Bohdán Katsenko <80700199+bohdan1203@users.noreply.github.com>
2021-09-02 20:11:45 +0300

    dsfsl

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { useState, useEffect } from "react";
+import { Bar, Doughnut, Line } from "react-chartjs-2";
+import "./App.css";
+
+
+
+function App() {
+    return (
+    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, voluptatem! Cumque cupiditate nostrum ducimus alias cum assumenda eius id, vitae explicabo, debitis eaque voluptas aperiam reprehenderit sunt vel error quo.</div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Switch, Route } from "react-router";
+import App from "./App";
+
+
+function Asdf() {
+  return 'ASDFGHJKL'
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <Switch>
+        <Route  path="/money"  component={App} />
+        <Route  path="/asdf"  component={Asdf} />
+      </Switch>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
