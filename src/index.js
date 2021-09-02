4d0e07ca0c42e7319511e70ae453f7bb5e18924b
Bohdán Katsenko <80700199+bohdan1203@users.noreply.github.com>
2021-09-02 21:00:32 +0300

    djksfl

--- src/index.js
+++ src/index.js
@@ -1,22 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
-import { Switch, Route } from "react-router";
 import App from "./App";
 
-
-function Asdf() {
-  return 'ASDFGHJKL'
-}
-
 ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <Switch>
-        <Route exact path="/money"  component={App} />
-        <Route exact path="/asdf"  component={Asdf} />
-      </Switch>
-    </Router>
-  </React.StrictMode>,
+  <App />,
   document.getElementById("root")
 );
