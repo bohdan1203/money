1374e3c3a5734f738d76ee1f54e8821c803e1f0b
Bohdán Katsenko <80700199+bohdan1203@users.noreply.github.com>
2021-09-02 22:16:10 +0300

    dkjfsdl

--- src/App.js
+++ src/App.js
@@ -24,10 +24,40 @@ const filterItems = {
 };
 
 function App() {
+  const input = document.getElementById("input");
+
+  const [modal, setModal] = useState(false);
+  const [operationType, setOperationType] = useState(null);
+  const [sum, setSum] = useState("");
+  const [selectType, setSelectType] = useState(null);
+  const [category, setCategory] = useState("Виберіть категорію");
+  const [date, setDate] = useState(new Date().toISOString().slice(0, 10));
+  const [description, setDescription] = useState("");
+  const [history, setHistory] = useState(data);
+  const [nevermind, setNevermind] = useState(false);
+  const [modalEdit, setModalEdit] = useState(false);
+  const [editId, setEditId] = useState(null);
+  const [areYouSure, setAreYouSure] = useState(false);
+  const [modalEditCategories, setModalEditCategories] = useState(false);
+  const [modalFilter, setModalFilter] = useState(false);
+
+  useEffect(() => {
+    localStorage.setItem("data", JSON.stringify(incomesExpenses));
+  });
+
+  function currentBalance() {
+    let balance = 0;
+    for (let i = 0; i < incomesExpenses.length; i++) {
+      balance += incomesExpenses[i].sum;
+    }
+    return Math.round(balance * 100) / 100;
+  }
+
   return (
     <div>
       <h1>{ooo}</h1>
       <h2>{categoriesIncomesSelect[0]}</h2>
+      <h1>Ваш баланс: {currentBalance()} грн.</h1>
     </div>
   )
 }
